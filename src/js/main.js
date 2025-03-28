b5db783186fb6b95752995c0b4a6a4a8cd528f07
Hammadniazi <hammadniazi991@gmail.com>
2025-03-28 14:44:44 +0100

    just clean and remove the extra commented code

--- src/js/main.js
+++ src/js/main.js
@@ -125,7 +125,6 @@ document.addEventListener("DOMContentLoaded", () => {
   loadFavorites();
 });
 
-
 // Show notification function
 function showNotification(message, type = "info") {
   // Create notification element
@@ -759,31 +758,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }, 1000);
 });
-
-// import "./style.css";
-// import javascriptLogo from "./javascript.svg";
-// import viteLogo from "/vite.svg";
-// import { setupCounter } from "../counter.js";
-
-// document.querySelector("#app").innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `;
-
-// setupCounter(document.querySelector("#counter"));
 
 // Footer
 
